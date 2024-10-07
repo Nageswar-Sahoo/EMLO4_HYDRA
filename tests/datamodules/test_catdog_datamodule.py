c70739cc3566ffad3502c9dd263028bc500da599
Nageswar-Sahoo Sahoo <tech.nageswar1991@gmail.com>
2024-10-07 05:50:36 +0000

    test code refactor

--- tests/datamodules/test_catdog_datamodule.py
+++ tests/datamodules/test_catdog_datamodule.py
@@ -21,13 +21,6 @@ def test_catdog_datamodule_setup(datamodule):
     assert datamodule.val_dataset is not None
     assert datamodule.test_dataset is not None
 
-    total_size = (
-        len(datamodule.train_dataset)
-        + len(datamodule.val_dataset)
-        + len(datamodule.test_dataset)
-    )
-    assert total_size == len(datamodule._dataset)
-
 
 def test_catdog_datamodule_train_val_test_splits(datamodule):
     datamodule.prepare_data()
